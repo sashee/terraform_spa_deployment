e996f055f829f38c5e5a3870de30acf10699d255
Tamas Sallai <gsashee@gmail.com>
2019-10-11 17:54:06 +0200

    format

--- main.tf
+++ main.tf
@@ -2,45 +2,45 @@ provider "aws" {
 }
 
 terraform {
-	required_version = ">= 0.12.8"
+  required_version = ">= 0.12.8"
 }
 
 # frontend
 
 resource "aws_s3_bucket" "frontend_bucket" {
-	force_destroy = "true"
-	website {
-		index_document = "index.html"
-	}
+  force_destroy = "true"
+  website {
+    index_document = "index.html"
+  }
 }
 
 data "external" "frontend_build" {
-	program = ["bash", "-c", <<EOT
+  program = ["bash", "-c", <<EOT
 (npm ci && npm run build -- --env.PARAM="$(jq -r '.param')") >&2 && echo "{\"dest\": \"dist\"}"
 EOT
-]
-	working_dir = "${path.module}/frontend"
-	query = {
-		param = "Hi from Terraform!"
-	}
+  ]
+  working_dir = "${path.module}/frontend"
+  query = {
+    param = "Hi from Terraform!"
+  }
 }
 
 locals {
-	mime_type_mappings = {
-		html="text/html",
-		js="application/javascript",
-		css="text/css"
-	}
+  mime_type_mappings = {
+    html = "text/html",
+    js   = "application/javascript",
+    css  = "text/css"
+  }
 }
 
 resource "aws_s3_bucket_object" "frontend_object" {
   for_each = fileset("${data.external.frontend_build.working_dir}/${data.external.frontend_build.result.dest}", "*")
 
-  key    = each.value
-  source = "${data.external.frontend_build.working_dir}/${data.external.frontend_build.result.dest}/${each.value}"
-  bucket = aws_s3_bucket.frontend_bucket.bucket
-  etag = filemd5("${data.external.frontend_build.working_dir}/${data.external.frontend_build.result.dest}/${each.value}")
-	content_type = lookup(local.mime_type_mappings, concat(regexall("\\.([^\\.]*)$", each.value),[[""]])[0][0], "application/octet-stream")
+  key          = each.value
+  source       = "${data.external.frontend_build.working_dir}/${data.external.frontend_build.result.dest}/${each.value}"
+  bucket       = aws_s3_bucket.frontend_bucket.bucket
+  etag         = filemd5("${data.external.frontend_build.working_dir}/${data.external.frontend_build.result.dest}/${each.value}")
+  content_type = lookup(local.mime_type_mappings, concat(regexall("\\.([^\\.]*)$", each.value), [[""]])[0][0], "application/octet-stream")
 }
 
 # Boilerplate for the bucket
